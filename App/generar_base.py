0d54d693504272e8129c051bde382097a4aa9117
ikestrella <ikestrella@utpl.edu.ec>
2025-05-21 09:54:20 -0500

    Taller

---
+++ App/generar_base.py
@@ -0,0 +1,58 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from configuracion import cadena_base_datos
+
+engine = create_engine(cadena_base_datos)
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__= "usuario"
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+
+    publicaciones = relationship("Publicacion", back_populates="usuario")
+    reacciones = relationship("Reaccion", back_populates="usuario")
+
+    def __repr__(self):
+        return "Usuario: publicacion=%s\n reaccion=%s\n"% (
+                          self.publicacion,
+                          self.reaccion)
+
+
+class Publicacion(Base):
+    __tablename__= "publicacion" 
+    id = Column(Integer, primary_key=True)
+    publicacion = Column(String())
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+
+    usuario = relationship("Usuario", back_populates="publicaciones")
+    reacciones = relationship("Reaccion", back_populates="publicacion")
+
+    def __repr__(self):
+        return "Publicacion: publicacion=%s"% (
+                          self.publicacion)
+
+      
+    
+
+class Reaccion(Base):
+    __tablename__= "reaccion"
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), primary_key=True)
+    publicacion_id = Column(Integer, ForeignKey('publicacion.id'),primary_key=True)
+    tipo_emocion = Column(String())
+
+
+    publicacion = relationship("Publicacion", back_populates="reacciones")
+    usuario = relationship("Usuario", back_populates="reacciones")
+
+    def __repr__(self):
+        return "Reaccion: publicacion=%s\n reaccion=%s\n"% (
+                          self.publicacion,
+                          self.reaccion)
+
+Base.metadata.create_all(engine)
---
+++ App/ingresar_data.py
@@ -0,0 +1,47 @@
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from generar_base import *
+
+from configuracion import cadena_base_datos
+
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+csv1 = "../DATA/usuarios_red_x.csv"
+
+with open(csv1, mode='r', encoding='utf-8', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader)
+    
+    for row in csv_reader:
+        usuario = Usuario(nombre=row[0].strip())
+        session.add(usuario)
+    session.commit()
+
+
+csv2 = "../DATA/usuarios_publicaciones.csv"
+with open(csv2, mode='r', encoding='utf-8', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter="|")
+    next(csv_reader)
+    
+    for row in csv_reader:
+        usuario = session.query(Usuario).filter(Usuario.nombre==row[0].strip()).one()
+        publicacion = Publicacion(usuario=usuario, publicacion=row[1])
+        session.add(publicacion)
+    session.commit()
+
+csv23 = "../DATA/publicaciones_liga_premier.csv"
+with open(csv2, mode='r', encoding='utf-8', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter="|")
+    next(csv_reader)
+    
+    for row in csv_reader:
+        usuario = session.query(Usuario).filter(Usuario.nombre==row[0].strip()).one()
+        publicacion = Publicacion(usuario=usuario, publicacion=row[1])
+        session.add(publicacion)
+    session.commit()
